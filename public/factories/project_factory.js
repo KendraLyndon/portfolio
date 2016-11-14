20f3a6223ee7e3f0f19377c5da7957704a152d96
Kendra Lyndon <kendralyndon@gmail.com>
2016-11-14 12:12:50 -0700

    redo styling

--- public/factories/project_factory.js
+++ public/factories/project_factory.js
@@ -3,9 +3,10 @@ app.factory('ProjectService', function(){
     all : [
       {
         name: 'Pianissimo',
-        images: ['../images/PianissimoSplash.jpg','../images/PianissimoKeyboard.jpg'],
+        first: true,
+        images: ['../images/PianissimoSplash.jpg'],
         video: 'https://www.youtube.com/embed/rJhstTNNCgM',
-        tech: 'Android Studio  /  Java  /  XML',
+        tech: 'Android Studio  |  Java  |  XML',
         liveLink: 'https://play.google.com/store/apps/details?id=com.kendralyndon.pianopractice&hl=en',
         gitHub: 'https://github.com/KendraLyndon/Pianissimo',
         blurb: 'Available in the Google Play app store! '+
@@ -14,10 +15,10 @@ app.factory('ProjectService', function(){
                '88 keys scrollable within the app.'
       },
       {
-        name: 'Coffee Snob',
-        images: ['../images/coffee-snob-map.png'],
+        name: 'CoffeeSnob',
+        images: ['../images/coffee-snob-scrnsht.png'],
         video: null,
-        tech: 'Node.js  /  Express  /  Jade  /  PostgreSQL  /  Google Maps API  /  custom CSS',
+        tech: 'Node.js  |  Express  |  Jade  |  PostgreSQL  |  Google Maps API',
         liveLink: 'http://coffeesnob-kl.herokuapp.com/',
         gitHub: 'https://github.com/KendraLyndon/Coffee-Snob',
         blurb: 'Web-based app where users can find local coffee shops '+
@@ -25,9 +26,9 @@ app.factory('ProjectService', function(){
       },
       {
         name: 'Shoptacular',
-        images: ['../images/shoptacular-screenshot.png'],
+        images: ['../images/shoptacular-scrnsht.png'],
         video: null,
-        tech: 'AngularJS  /  BootstrapCSS',
+        tech: 'AngularJS  |  BootstrapCSS',
         liveLink: 'https://shopify-fdcbf.firebaseapp.com/',
         gitHub: 'https://github.com/KendraLyndon/MEANtea',
         blurb: 'E-commerce shopping cart where users can add items and checkout.'
