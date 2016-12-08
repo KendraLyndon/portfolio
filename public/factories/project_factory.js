272d186067263c8937a72c23b0f97aee42cf1816
Kendra Lyndon <kendralyndon@gmail.com>
2016-12-07 17:49:24 -0700

    edit coffeesnob tech

--- public/factories/project_factory.js
+++ public/factories/project_factory.js
@@ -6,7 +6,7 @@ app.factory('ProjectService', function(){
         first: true,
         images: ['../images/PianissimoSplash.jpg'],
         video: 'https://www.youtube.com/embed/rJhstTNNCgM',
-        tech: 'Android Studio  |  Java  |  XML',
+        tech: 'Android Studio | Java | XML',
         liveLink: 'https://play.google.com/store/apps/details?id=com.kendralyndon.pianopractice&hl=en',
         gitHub: 'https://github.com/KendraLyndon/Pianissimo',
         blurb: 'Native app featuring an interactive piano, available in '+
@@ -18,7 +18,7 @@ app.factory('ProjectService', function(){
         name: 'CoffeeSnob',
         images: ['../images/coffee-snob-scrnsht.png'],
         video: null,
-        tech: 'Node.js  |  Express  |  Jade  |  PostgreSQL  |  Google Maps',
+        tech: 'Node.js | Express | PostgreSQL | Google Maps',
         liveLink: 'http://coffeesnob-kl.herokuapp.com/',
         gitHub: 'https://github.com/KendraLyndon/Coffee-Snob',
         blurb: 'Web-based app where users can find local coffee shops '+
@@ -28,7 +28,7 @@ app.factory('ProjectService', function(){
         name: 'Shoptacular',
         images: ['../images/shoptacular-scrnsht.png'],
         video: null,
-        tech: 'AngularJS  |  BootstrapCSS',
+        tech: 'AngularJS | BootstrapCSS',
         liveLink: 'https://shopify-fdcbf.firebaseapp.com/',
         gitHub: 'https://github.com/KendraLyndon/MEANtea',
         blurb: 'E-commerce shopping cart where users can add items and checkout.'
